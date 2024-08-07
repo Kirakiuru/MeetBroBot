2dbfb4df08c586148be86264dc211480e8a87e43
Kirll-Vinogradov <vinogradov.kirill@lab-company.ru>
2024-08-08 01:17:53 +0300

    Убрал лишнее, оставил только основу

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import asyncio
-# import logging
 from aiogram import Bot, Dispatcher
-# from aiogram.enums.parse_mode import ParseMode
-# from aiogram.fsm.storage.memory import MemoryStorage
 
 from app.handlers import router
 from config import BOT_TOKEN
@@ -10,18 +7,16 @@ from config import BOT_TOKEN
 
 async def main():
     print('Бот запущен')
-    bot = Bot(token=BOT_TOKEN) # parse_mode=ParseMode.HTML
+    bot = Bot(token=BOT_TOKEN)
     # обработчик
-    dp = Dispatcher() # storage=MemoryStorage()
+    dp = Dispatcher()
     # подключаем импортированный роутер из другого файла
     dp.include_router(router)
-    # await bot.delete_webhook(drop_pending_updates=True)
     # начинает полинг = опрашивает сервер телеграм
-    await dp.start_polling(bot) # allowed_updates=dp.resolve_used_update_types()
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    # logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
